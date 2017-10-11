00db208bdf954c62c7dbf0e56aa81bd992e5ec60
kineco42 <kineco@gmail.com>
2017-10-11 11:00:34 -0300

    created project and made a rational numbers class called rational.cpp with the header file rational.h

---
+++ Assignmnet2/rational.cpp
@@ -0,0 +1,5 @@
+//
+// Created by prog2100 on 11/10/17.
+//
+
+#include "rational.h"
---
+++ Assignmnet2/rational.h
@@ -0,0 +1,14 @@
+//
+// Created by prog2100 on 11/10/17.
+//
+
+#ifndef ASSIGNMNET2_RATIONAL_H
+#define ASSIGNMNET2_RATIONAL_H
+
+
+class rational {
+
+};
+
+
+#endif //ASSIGNMNET2_RATIONAL_H
