f31ee0023fed0a49d2e4b094d81d56ee8ba0a0a4
kineco42 <kineco@gmail.com>
2017-11-15 10:44:58 -0400

    Created project

---
+++ HTMLConverter/converter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by prog2100 on 15/11/17.
+//
+
+#include "converter.h"
---
+++ HTMLConverter/converter.h
@@ -0,0 +1,14 @@
+//
+// Created by prog2100 on 15/11/17.
+//
+
+#ifndef HTMLCONVERTER_CONVERTER_H
+#define HTMLCONVERTER_CONVERTER_H
+
+
+class converter {
+
+};
+
+
+#endif //HTMLCONVERTER_CONVERTER_H
